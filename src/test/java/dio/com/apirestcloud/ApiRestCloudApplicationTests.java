0e1de14c369bcb4273f223e289be3ac6e9d0d93a
Gabriel Braga <gabrielsouza1170@gmail.com>
2025-03-26 23:19:24 -0300

    Initial commit

---
+++ src/test/java/dio/com/apirestcloud/ApiRestCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.com.apirestcloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestCloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
